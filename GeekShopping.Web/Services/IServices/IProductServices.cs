3b05af12e05d6a82a348af8fbe2ada01b0772480
Fernando José Dos Anjos <fernandoanjos0202@gmail.com>
2022-06-11 20:23:31 -0300

    Adicionei a url do produto no AppSettings, criei a classe ProductModel na API WEB, criei a interface do produto, criei o utilitário httpClientExtensions e adicionei o método ReadContentAs.

---
+++ GeekShopping.Web/Services/IServices/IProductServices.cs
@@ -0,0 +1,13 @@
+﻿using GeekShopping.Web.Models;
+
+namespace GeekShopping.Web.Services.IServices
+{
+    public interface IProductServices
+    {
+        Task<IEnumerable<ProductModel>> FindAllProducts();
+        Task<ProductModel> FindProductById(int id);
+        Task<ProductModel> Create(ProductModel product);
+        Task<ProductModel> Update(ProductModel product);
+        Task<bool> Delete(int id);
+    }
+}
---
+++ GeekShopping.Web/Utils/HttpClientExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+
+namespace GeekShopping.Web.Utils
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
+        {
+            if (!response.IsSuccessStatusCode)
+                throw new ApplicationException($"Algo deu errado: {response.ReasonPhrase}");
+            var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return JsonSerializer.Deserialize<T>(content,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = false });
+        }
+    }
+}
